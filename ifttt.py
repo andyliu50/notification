6fb165641d5865e90e064d6ff665c6b0fe65ea1c
Andy Liu <andyliu60@outlook.com>
2019-10-25 21:52:34 +0800

    modified

--- ifttt.py
+++ ifttt.py
@@ -11,12 +11,22 @@ class Notification(object):
     
 	mykey = 'dVNxHrwtqldbtVk8OjbsDs'
 	def __init__(self, event, **values):
-		self.url = "https://maker.ifttt.com/trigger/%s/with/key/%s" % (event, self.mykey)
-		self.report = values
+		self.__url = "https://maker.ifttt.com/trigger/%s/with/key/%s" % (event, self.mykey)
+		self.__values = values
 	
 	def sent(self):
-		requests.post(self.url, data=self.report)
+		requests.post(self.__url, data=self.__values)
+
+	@property
+	def url(self):
+		return self.__url
+
+	@property
+	def values(self):
+		return self.__values
 
 if __name__ == '__main__':
 	n = Notification('op-answers', value1 = 'answer1', value2 = 'answer2', value3 = 'answer3')
-	n.sent()
+#	n.sent()
+	print(n.url)
+	print(n.values)
